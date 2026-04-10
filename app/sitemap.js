09948cefb9632c8c58351b95d4571941522b5625
Titan X Dev <inkmcontop@gmail.com>
2026-04-10 23:46:30 +0530

    Add new URLs to sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -6,5 +6,23 @@ export default function sitemap() {
       changeFrequency: "daily",
       priority: 1,
     },
+    {
+      url: "https://lavalink.vexanode.cloud/ssl",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: "https://lavalink.vexanode.cloud/non-ssl",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: "https://lavalink.vexanode.cloud/submit",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
   ];
 }
